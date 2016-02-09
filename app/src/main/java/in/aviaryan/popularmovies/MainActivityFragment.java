33ff62cba8368af4d554804e84e937b156cd324a
aviaryan <avi.aryan123@gmail.com>
2016-02-09 15:48:55 +0530

    setup picasso

--- app/src/main/java/in/aviaryan/popularmovies/MainActivityFragment.java
+++ app/src/main/java/in/aviaryan/popularmovies/MainActivityFragment.java
@@ -2,14 +2,21 @@ package in.aviaryan.popularmovies;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private View mainFragementView;
+    public String LOG_TAG = "ShowcaseFragement";
 
     public MainActivityFragment() {
     }
@@ -17,6 +24,14 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        mainFragementView = inflater.inflate(R.layout.fragment_main, container, false);
+        // load image
+        Picasso.with(getContext()).load("http://i.imgur.com/DvpvklR.png").into(
+                (ImageView) mainFragementView.findViewById(R.id.imageView1) );
+        Picasso.with(getContext()).load("http://i.imgur.com/DvpvklR.png").into(
+                (ImageView) mainFragementView.findViewById(R.id.imageView2) );
+
+        Log.v(LOG_TAG, "Image loaded");
+        return mainFragementView;
     }
 }
